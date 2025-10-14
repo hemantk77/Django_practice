f49e0ee7f9840a4aeafab7a7a5ef9565a26e1f35
hemantk77 <hemant11uk@gmail.com>
2025-10-14 23:16:37 +0200

    add function updated with the correct name of the variables

--- django_practice/tasks/views.py
+++ django_practice/tasks/views.py
@@ -5,7 +5,7 @@ tasks = ["foo", "bar", "baz"]
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
@@ -17,7 +17,7 @@ def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            form.cleaned_data["task"]
+            task = form.cleaned_data["task"]
             tasks.append(task)
         else:
             return render(request, "tasks/add.html", {
